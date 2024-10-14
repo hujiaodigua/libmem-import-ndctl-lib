e4f9708669b754bce8db7dbdb23b2636a1b0f697
yeeli <seven.yi.lee@gmail.com>
2024-10-14 19:17:12 +0800

    build and compile OK
    Signed-off-by: yeeli <seven.yi.lee@gmail.com>

---
+++ build/CMakeFiles/mem-cli.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yeeli/Yee-HDD/qemu-cxl_and_kernel/JackrabbitLabs/libmem/src/cli.c" "CMakeFiles/mem-cli.dir/src/cli.c.o" "gcc" "CMakeFiles/mem-cli.dir/src/cli.c.o.d"
+  "/home/yeeli/Yee-HDD/qemu-cxl_and_kernel/JackrabbitLabs/libmem/src/libmem.c" "CMakeFiles/mem-cli.dir/src/libmem.c.o" "gcc" "CMakeFiles/mem-cli.dir/src/libmem.c.o.d"
+  "/home/yeeli/Yee-HDD/qemu-cxl_and_kernel/JackrabbitLabs/libmem/src/log.c" "CMakeFiles/mem-cli.dir/src/log.c.o" "gcc" "CMakeFiles/mem-cli.dir/src/log.c.o.d"
+  "/home/yeeli/Yee-HDD/qemu-cxl_and_kernel/JackrabbitLabs/libmem/src/options.c" "CMakeFiles/mem-cli.dir/src/options.c.o" "gcc" "CMakeFiles/mem-cli.dir/src/options.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mem-cli.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mem-cli.dir/src/cli.c.o"
+  "CMakeFiles/mem-cli.dir/src/cli.c.o.d"
+  "CMakeFiles/mem-cli.dir/src/libmem.c.o"
+  "CMakeFiles/mem-cli.dir/src/libmem.c.o.d"
+  "CMakeFiles/mem-cli.dir/src/log.c.o"
+  "CMakeFiles/mem-cli.dir/src/log.c.o.d"
+  "CMakeFiles/mem-cli.dir/src/options.c.o"
+  "CMakeFiles/mem-cli.dir/src/options.c.o.d"
+  "mem-cli"
+  "mem-cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mem-cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
